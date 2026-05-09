2ae4282b29d90236e70f41063cacfd80cd387fd7
ViniDev123 <viniciusdaniel13@outlook.com>
2026-05-09 17:47:28 -0300

    fix: trust proxy para Render

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const path        = require('path');
 const connectDB   = require('./config/db');
 
 const app = express();
+app.set('trust proxy', 1);
 connectDB();
 
 // ── CORS ─────────────────────────────────────────────────────
